d952bb29d40238d0ada40ff7dccfcf34f7d1619a
pranav550 <pranavverma550>
2020-03-30 13:52:29 +0530

    timezone change

--- src/app/featureModule/timezone/timezone-change/timezone-change.component.ts
+++ src/app/featureModule/timezone/timezone-change/timezone-change.component.ts
@@ -6,19 +6,32 @@ import * as moment from 'moment-timezone';
   styleUrls: ['./timezone-change.component.css']
 })
 export class TimezoneChangeComponent implements OnInit {
-  public selectedMoment = new Date();
-  constructor() { 
+  public selectedMoment:any ;
+  timez:any=[];
+  
+  constructor( ) { 
    
   }
 
   ngOnInit(): void {
-    console.log(moment().utcOffset("+05:30").format(), "IST")//ist
-    console.log(moment().utcOffset("-08:00").format(),"PST")//pst
-    console.log(moment().utcOffset("-07:00").format(),"mountain time")//mountain time
-    console.log(moment().utc().format(),"utc")//utc
-    console.log(moment().utcOffset('-06:00').format(),"cst")// cst
+  this.getTimezone()
   }
-  // test = [new Date(), new Date()];
 
+  //function for get timezone
+  getTimezone(){
+    
+    this.timez=[
+    {id:1,name:"IST", time:moment(this.selectedMoment).utcOffset("+05:30").format("M/DD/YYYY, hh:mm A")},
+    {id:2,name:"PST", time:moment(this.selectedMoment).utcOffset("-08:00").format("M/DD/YYYY, hh:mm A")},
+    {id:3,name:"Mountain Time", time:moment(this.selectedMoment).utcOffset("-07:00").format("M/DD/YYYY, hh:mm A")},
+    {id:4,name:"UTC", time:moment(this.selectedMoment).utc().format("M/DD/YYYY, hh:mm A")},
+    {id:5,name:"CST", time:moment(this.selectedMoment).utcOffset('-06:00').format("M/DD/YYYY, hh:mm A" )},
+  ]
+  }
+  // function for change date
+  selectedDate(val){
+    this.selectedMoment = val;
+    this.getTimezone()
+  }
 
 }
